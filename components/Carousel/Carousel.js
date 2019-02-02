92d03d471c3f0d704ccd69208748fab76c52f79c
Pav0l <pavol.karas@gmail.com>
2019-02-03 00:20:45 +0100

    Rework carousel functionality

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,68 +1,30 @@
+// Grab references for carousel images and carousel.
+let carouselImg = document.querySelectorAll('.carousel img');
+let carousel = document.querySelector('.carousel');
+// Add 'click' listeners to carousel buttons
+let leftBtn = carousel.firstElementChild.addEventListener('click', () => slide(-1));
+let rightBtn = carousel.lastElementChild.addEventListener('click', () => slide(1));
+// Set current position to 1 - current 'display-img' position based on html data-count attribute
+let currentPosition = 1;
 
-class Carousel {
-    constructor(image) {
-        this.image = image;
-        this.dataCount = this.image.dataset.count;
-        
-        leftBtn.addEventListener('click', () => this.slideLeft());
-        rightBtn.addEventListener('click', () => this.slideRight());
+function slide(num) {
+    // Check with Node List item (position) has 'display-img' class
+    for (let i = 0; i < carouselImg.length; i++) {
+        if (carouselImg[i].classList.contains('display-img')) {
+            // update current position based on Node list data-count attribute change position +1/-1
+            currentPosition = Number(carouselImg[i].dataset.count)+Number(num);
+            carouselImg[i].classList.remove('display-img');
+            // check if current position did not go over Node list length or equal to 0
+            if (currentPosition > carouselImg.length) {
+                currentPosition = 1;
+            } else if (currentPosition <= 0) {
+                currentPosition = carouselImg.length;
+            }
+            // Add 'display-img' to next item based on data-count attribute
+            let nextItem = document.querySelector(`.carousel img[data-count="${currentPosition}"]`);
+            nextItem.classList.add('display-img');
+            return currentPosition;
+        }
     }
-
-    slideLeft() {
-        console.log('currPos-left-start: ', currentPosition);
-
-        if (this.image.classList.contains('display-img')) {
-            currentPosition = Number(this.dataCount);
-            this.image.classList.remove('display-img')
-        }
-        console.log('currPos-left-after-for: ', currentPosition);
-        currentPosition = currentPosition - 1;
-        if (currentPosition <= 0) {
-            currentPosition = carouselImg.length;
-        }
-        console.log('currPos-left-after-if-into-next: ', currentPosition);
-        let nextItem = document.querySelector(`.carousel img[data-count="${currentPosition}"]`);
-        console.log('nextItem-after-curPos: ', nextItem);
-        nextItem.classList.add('display-img');
-    }
-
-    slideRight() {
-        console.log('currPos-right-start: ', currentPosition);
-
-        if (this.image.classList.contains('display-img')) {
-            currentPosition = Number(this.dataCount);
-            this.image.classList.remove('display-img')
-        }
-        console.log('currPos-right-after-for: ', currentPosition);
-        currentPosition = currentPosition + 1;
-        console.log('carouselImg length', carouselImg.length)
-        if (currentPosition > carouselImg.length) {
-            currentPosition = 1;
-        }
-        console.log('currPos-right-after-if-into-next: ', currentPosition);
-        let nextItem = document.querySelector(`.carousel img[data-count="${currentPosition}"]`);
-        console.log('nextItem-after-curPos: ', nextItem);
-        nextItem.classList.add('display-img');
-    }
-
 }
 
-let carousel = document.querySelector('.carousel');
-let leftBtn = carousel.firstElementChild;
-let rightBtn = carousel.lastElementChild;
-let currentPosition;
-
-
-let carouselImg = document.querySelectorAll('.carousel img');
-carouselImg.forEach(img => new Carousel(img));
-
-
-
-/* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
-    2. You will need to grab a reference to all of the images
-    3. Create a current index
-    4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
-    6. Have fun!
-*/
